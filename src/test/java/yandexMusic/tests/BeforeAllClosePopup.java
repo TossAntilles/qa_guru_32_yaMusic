cfc9fbbffa873482c7d61856d38adbe115079a20
Anton Bogutskii <schicksalkreuzung@gmail.com>
2025-02-03 01:08:43 +0300

    connection to cloud

--- src/test/java/yandexMusic/tests/BeforeAllClosePopup.java
+++ src/test/java/yandexMusic/tests/BeforeAllClosePopup.java
@@ -2,6 +2,9 @@ package yandexMusic.tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -11,10 +14,21 @@ public class BeforeAllClosePopup {
 
     @BeforeAll
     public static void beforeAll(){
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        Configuration.browser = "chrome";
+        //Configuration.browserVersion = "124.0";
+
         Configuration.pageLoadStrategy = "eager";
         open("https://music.yandex.ru/");
         refresh();
-        Configuration.remote = "https://allure8:allure8@selenoid.autotests.cloud/wd/hub";
 
         //set language
         $(".d-lang-switcher").scrollIntoView(true).click();
